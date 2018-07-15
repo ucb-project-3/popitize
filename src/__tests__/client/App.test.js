b234c48e2e44ce15902b53454dd34e95a201d7f7
Benjamin <rose.benjamin925@gmail.com>
2018-07-15 16:31:52 -0700

    gulpfile made and npm scripts updated

--- src/__tests__/client/App.test.js
+++ src/__tests__/client/App.test.js
@@ -1,17 +1,23 @@
+// verify that App component mounts
 import React from 'react';
 import { expect } from 'chai';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { spy } from 'sinon';
 import App from '../../client/App';
 
+App.prototype.componentWillUnmount = spy();
+App.prototype.componentDidMount = spy();
 
-spy(App.prototype, 'componentDidMount');
-
+const wrapper = mount(<App />);
+wrapper.unmount();
 
 describe('<App />', () => {
   it('should call "componentDidMount"', (done) => {
-    const wrapper = mount(<App />);
     expect(App.prototype.componentDidMount.calledOnce).to.equal(true);
+    done();
+  });
+  it('should call "componentWillUnmout"', (done) => {
+    expect(App.prototype.componentWillUnmount.calledOnce).to.equal(true);
     done();
   });
 });
