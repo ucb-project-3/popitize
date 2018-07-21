9e714b5337abad4f3331ce4a3a4c4e2c3c118954
yaliu8 <yaliu8@gmail.com>
2018-07-21 16:13:36 -0700

    added user, host, renter seed data

---
+++ src/server/db/user.sql
@@ -0,0 +1,13 @@
+INSERT INTO User (email, first_name, last_name, password, age_range, credit_rating, address_1, city, state, zip, address_2)
+
+VALUES 
+("ben@gmail.com", "Ben", "Octopus", "12345", 60, 800, "620 2nd street", "San Francisco", "CA", 94107), 
+("james@gmail.com", "James", "Kanye", "kanyeisgreat123", 25, 750, "1001 Pine street", "San Francisco", "CA", 94102), 
+("anna@gmail.com", "Anna", "Beyonce", "ilovesalesforce", 18, 600, "1445 Bush Street", "San Francisco", "CA", 94109), 
+("sevil@gmail.com", "Sevil", "Drake", "drakeisgreat", 35, 700, "640 2nd street", "San Francisco", "CA", 94107), 
+("alice@gmail.com", "Alice", "Teddy", "woofwoof", 40, 500, "2225 23rd street", "San Francisco", "CA", 94107), 
+("david@gmail.com", "David", "Beer", "gluglug", 60, 600, "1628 Hostetter Road", "San Jose", "CA", 95131), 
+("raiben@gmail.com", "Rai", "Man", "shotsoclock", 60, 800, "151 3rd street", "San Francisco", "CA", 94103), 
+("emma@gmail.com", "Emma", "TA", "rockin", 60, 680, "620 2nd street", "San Francisco", "CA", 94107), 
+("coop@gmail.com", "Coop", "Sub", "iteachyou123", 60, 640, "170 O'Farrell Street", "San Francisco", "CA", 94102), 
+("berkeley@gmail.com", "Berkeley", "Hamster", "unaccountablehamsters", 60, 720, "160 Main street", "San Francisco", "CA", 94102), 
