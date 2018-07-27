babddfe4ca176396ff48bf7bc7ae242b70b8e84e
Benjamin <rose.benjamin925@gmail.com>
2018-07-27 14:35:31 -0700

    fixed static path for react app for production mode

--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const parser = require('body-parser');
 const morgan = require('morgan');
 const db = require('./models');
-const authRoutes = require('./controllers/authenticationRoutes')
+const authRoutes = require('./controllers/authenticationRoutes');
 const userSeed = require('./seeders/seeds');
+const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -13,7 +14,7 @@ app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 app.use(parser.text());
 
-app.use('/', express.static('../../build'));
+app.use('/', express.static(path.join(__dirname, '../../dist')));
 app.use(authRoutes);
 
 if (!(process.env.NODE_ENV === 'test')) {
