381e8e962735bcdbf973199510234760b4c5f810
benoctopus <rose.benjamin925@gmail.com>
2018-08-08 21:18:50 -0700

    host grid mobile responsive

--- src/client/presentational/HostGrid.js
+++ src/client/presentational/HostGrid.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-
+import { Hidden } from '@material-ui/core';
 
 const HostGrid = ({
   row1,
@@ -8,24 +8,29 @@ const HostGrid = ({
   row3,
   row4,
 }) => (
-  <div id="host-grid">
-    <div style={{ gridColumn: 1 }} className="host-grid-row">
-      {row1}
-    </div>
-    <div style={{ gridColumn: 2 }} className="host-grid-row">
-      {row2}
-    </div>
-    <div style={{ gridColumn: 3 }} className="host-grid-row">
-      {row3}
-    </div>
-    <div style={{ gridColumn: 4 }} className="host-grid-row">
-      {row4}
-    </div>
-    {/* <div style={{ gridColumn: 5 }} className="host-grid-row">
-      {row5}
-    </div> */}
-
-  </div>
+  <Fragment>
+    <Hidden mdDown>
+      <div id="host-grid">
+        <div style={{ gridColumn: 1 }} className="host-grid-row">
+          {row1}
+        </div>
+        <div style={{ gridColumn: 2 }} className="host-grid-row">
+          {row2}
+        </div>
+        <div style={{ gridColumn: 3 }} className="host-grid-row">
+          {row3}
+        </div>
+        <div style={{ gridColumn: 4 }} className="host-grid-row">
+          {row4}
+        </div>
+      </div>
+    </Hidden>
+    <Hidden lgUp>
+      <div id="host-non-grid">
+        {[...row1, ...row2, ...row3, ...row4]}
+      </div>
+    </Hidden>
+  </Fragment>
 );
 
 // HostGrid.PropTypes = {
